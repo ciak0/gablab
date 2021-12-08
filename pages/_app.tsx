c08f08c19a631adc67e1cf62cbee67b8a5504066
Gabriele Ferrari <ciak0mc@gmail.com>
2021-12-08 12:23:36 +0100

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const i = 2;
 const Gablab = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 );
