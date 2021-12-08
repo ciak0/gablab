5279aa03ec6231aa8bb1502f14a7fa3c207aeb89
Gabriele Ferrari <ciak0mc@gmail.com>
2021-12-08 12:21:47 +0100

    Husky test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const i = 0;
+const i = 2;
 const Gablab = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 );
